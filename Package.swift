c60a3c02b1cdd21616b0055594940929a040fb63
Creator William <>
2019-09-27 16:06:49 +0800

    调整为静态库

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "ImageKit",
-            type: .dynamic,
+            type: .static,
             targets: ["ImageKit"]),
     ],
     dependencies: [
